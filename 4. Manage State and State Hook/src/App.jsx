8ae61d55e5ccf660d985a80447f9374d9a54d12b
arjulrao <arjulrao21@gmail.com>
2025-09-26 14:26:45 +0530

    useState Array and Object

--- 4. Manage State and State Hook/src/App.jsx
+++ 4. Manage State and State Hook/src/App.jsx
@@ -21,19 +21,105 @@ function App() {
     </div> 
   ) */
 
-
+/* 
   const [value, setValue] = useState({name: "Arjul Rao", isBand : false})
 
 
   return (
     <div className="p-4">
         <h1>Name : {value.name} </h1>
-        <h1>isBand : {value.isBand.toString()}</h1>
-        <button onClick={()=> setValue({...value, isBand: !value.isBand })}  className="px-3 py-1 bg-blue-500 rounded-full">Change Value</button>
+        <h1>isBand : {value.isBand.toString().toUpperCase()}</h1>
+        <button onClick={()=> setValue({...value, isBand: !value.isBand })}  className={`px-3 py-1 ${value.isBand ? 'bg-blue-500' : 'bg-orange-500' } rounded-full`}>Change Value</button>
+    </div>
+  )
+*/
+/* 
+// use state trunt chozzo ko update nhii karta hai, 
+  const [val, setVal] = useState({name: "Arjul Rao", age : 24});
+
+  return (
+    <div>
+      <button onClick={()=>{ 
+        setVal({...val, gender: "Male"})
+        // Hamana yha par console kra hai magr isma gender add nhii hua hoga q ke jab pura function run honjaige yuska bad chiza update hoti hai
+        console.log(val);
+        }}>Click</button>
+    </div>
+  )
+*/
+/* 
+  const [val, setVal] = useState([1,2,3,4,5])
+
+  return (
+    <div className="p-4">
+      {val.map(items => <h1>{items}</h1>)}
+      <button
+        onClick={()=>setVal(()=> {
+         return val.filter((item, index) => index!=val.length-1 )
+        })}
+        className="px-2 py-2 text-xs text-white rounded-xl bg-blue-500" type="button">Remove Number</button>
+    </div>
+
+  ) 
+*/
+
+  // const [val, setVal] = useState([1, 2, 3, 4, 5, 6]);
+
+/*   return (
+    <div className="p-5">
+      {val.map(item => <h1>{item}</h1>)}
+      <button onClick={()=> {
+        setVal(()=> val.filter((item, index)=> index!=2 ))
+      }}
+        className="px-2 py-2 text-xs text-white rounded-xl bg-blue-500" type="button">Remove 2 Number</button>
+    </div>
+  )
+*/
+/*   return (
+    <div className="p-5">
+      {val.map(item => <h1>{item}</h1>)}
+      <button onClick={()=> {
+        setVal(()=> val.filter((item )=> item%2 == 0 ))
+      }}
+        className="px-2 py-2 text-xs text-white rounded-xl bg-blue-500" type="button">Remove 2 Number</button>
+    </div>
+  ) */
+
+
+/*     return (
+    <div className="p-5">
+      {val.map(item => <h1>{item}</h1>)}
+      <button onClick={()=> {
+        setVal([...val, 7])
+      }}
+        className="px-2 py-2 text-xs text-white rounded-xl bg-blue-500" type="button">Add Number</button>
+    </div>
+  )
+ */
+
+  const [val, setVal] = useState([
+    {name: "Arjul", age:23}, 
+    {name: "Moin", age: 20}, 
+    {name: "Hamid", age: 33}
+  ])
+
+  return(
+    <div className="p-5">
+      {val.map((item) => (
+        <div>
+          <h1>{item.name}</h1>
+          <h2>{item.age}</h2>
+        </div>
+      ))}
+      <button 
+        onClick={()=>
+          setVal(()=> val.map(item => item.name === "Moin" ? ({name: "Moin", age: 22}) : item))
+          }
+        className="px-2 py-2 text-xs text-white rounded-xl bg-blue-500" type="button">Click
+      </button>
     </div>
   )
 
 }
-
 
 export default App;
